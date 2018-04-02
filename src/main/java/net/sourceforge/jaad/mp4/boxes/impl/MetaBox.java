c7c9512f19a9da6a7c1abfb2f8b5dab8819ba334
dieterstueken <dieter.stueken@t-online.de>
2018-04-02 22:18:16 +0200

    Revert "MetaBox with 12 byte payload"
    This reverts commit e727658

--- src/main/java/net/sourceforge/jaad/mp4/boxes/impl/MetaBox.java
+++ src/main/java/net/sourceforge/jaad/mp4/boxes/impl/MetaBox.java
@@ -22,7 +22,6 @@ public class MetaBox extends FullBox {
 		long possibleType = in.peekBytes(8) & 0xFFFFFFFFL;
 		if(possibleType != BoxTypes.HANDLER_BOX){
 			super.decode(in);
-			in.skipBytes(8);
 		}
 		readChildren(in);
 	}
